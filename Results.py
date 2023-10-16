42d32266f26da8483e577881a6e783fc5194b62f
raph <orsi.raph@gmail.com>
2023-10-16 09:39:26 +0200

    doc: Add documentation for Results entity

--- Results.py
+++ Results.py
@@ -14,20 +14,47 @@ class Results:
 
     @classmethod
     def from_json(cls, json_data: dict) -> list[Results]:
+        """
+        Créer une liste de résultats à partir d'un dictionnaire
+
+        Args:
+            json_data: Dictionnaire décrivant une liste de résultats
+
+        Returns:
+            Liste d'objets Results contenant les valeurs du dictionnaire, et exlucant les résultats sans valeur
+        """
         results = []
         for item in json_data:
             countryiso3code = item.get('countryiso3code')
             date = item.get('date')
             value = item.get('value')
+
+            if value is None:
+                continue
+
             result = cls(countryiso3code, date, value)
             results.append(result)
         return results
 
     @classmethod
     def extract_results(cls, json_dict: dict) -> list[Results]:
+        """
+        Extrait les informations d'un dictionnaire json
+
+        Args:
+            json_dict: Dictionnaire représentant une liste d'objets Results
+        Returns:
+            Liste d'objet Results
+        """
         return Results.from_json(json_dict[1])
 
     def to_json(self) -> str:
+        """
+        Créer une représentation json de l'objet Results'
+
+        Returns:
+            Objet Results sous forme de chaîne json
+        """
         result_dict = {
             'countryiso3code': self.countryiso3code,
             'date': self.date,
