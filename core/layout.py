abc934b533d54704dde8464b6166581d53e25653
Ibramadi75 <ibrahim75madi@gmail.com>
2024-01-03 20:56:02 +0100

    feat: added styles and default values

--- core/layout.py
+++ core/layout.py
@@ -2,16 +2,25 @@ from dash import html, dcc
 
 
 def get_layout(available_countries):
+    default_selected_countries = ['France', 'Allemagne', 'Italie']  # Ajout des pays par défaut
+
     return html.Div([
-        html.H4('GES per country'),
-        html.P("Country:"),
-        dcc.Dropdown(id='country',
-                     options=[{'label': country, 'value': country} for country in available_countries],
-                     value=[],
-                     multi=True
-                     ),
-        dcc.Graph(id="graph"),
-        dcc.Graph(id="geo"),
-        dcc.Graph(id="pie"),
-        dcc.Graph(id="line"),
-    ])
+        html.H1('GES per country', style={'color': '#333', 'marginBottom': 20, 'font-family': 'Arial, sans-serif',
+                                          'text-align': 'center'}),
+        html.P("Country:", style={'fontSize': 18, 'font-family': 'Georgia, serif'}),
+        dcc.Dropdown(
+            id='country',
+            options=[{'label': country, 'value': country} for country in available_countries],
+            value=default_selected_countries,  # Définir les pays par défaut
+            multi=True,
+            style={'marginBottom': 20}
+        ),
+        dcc.Graph(id="graph", style={'backgroundColor': '#f9f9f9', 'marginBottom': 20}),
+
+        html.Div([
+            dcc.Graph(id="geo", style={'backgroundColor': '#ffffff', 'width': '40%', 'flex': 1}),
+            dcc.Graph(id="pie", style={'backgroundColor': '#f9f9f9', 'width': '40%', 'flex': 1}),
+        ], style={'marginBottom': 20, 'display': 'flex', 'justify-content': 'space-between'}),
+
+        dcc.Graph(id="line", style={'backgroundColor': '#ffffff', 'marginBottom': 20}),
+    ], style={'backgroundColor': '#f4f4f4', 'padding': '20px'})
